f61c55e48aa4f29d0b94155376a097446594f932
Mark Dickinson <dickinsm@gmail.com>
2014-09-13 20:54:19 +0100

    Update proofs for floor_spec; introduce two alternative characterizations.

--- floor_and_ceiling.v
+++ floor_and_ceiling.v
@@ -21,34 +21,32 @@ intros r_nonnegative r_less_than_b. clear r_bounded.
 remember (Z.div_eucl a b) as qr.
 rewrite (surjective_pairing qr) in *. clear Heqqr.
 remember (fst qr) as q. remember (snd qr) as r. clear Heqq. clear Heqr.
-clear qr.
+clear qr. rewrite a_eq_bqr. clear a a_eq_bqr.
 
 (* fix mismatch between b * q and n * b *)
-replace (b * q) with (q * b) in a_eq_bqr by auto with zarith.
 
 (* Now split our double implication n * b <= a <-> n <= q into
    two pieces, and prove each piece separately. *)
 split; intro H0.
 
 (* Prove that n <= q, given n * b <= a. *)
-assert (n < q + 1 -> n <= q) as H2 by auto with zarith. apply H2. clear H2.
-assert (n * b < q * b + b) as H1 by auto with zarith.
-replace (q * b + b) with ((q + 1) * b) in H1 by ring.
-apply (Z.mul_lt_mono_pos_r _ _ _ b_is_positive). assumption.
+assert (n < q + 1).
+apply Zmult_lt_reg_r with (p := b). assumption.
+apply Zle_lt_trans with (m := b * q + r). assumption.
+replace ((q + 1) * b) with (b * q + b) by ring.
+auto with zarith. auto with zarith.
 
-(* Prove that n * b <= a, given n <= q. *)
-rewrite a_eq_bqr. clear a_eq_bqr.
-assert (q * b <= q * b + r). auto with zarith.
-assert (n * b <= q * b). apply Z.mul_le_mono_pos_r. assumption. assumption.
-auto with zarith.
+apply Zle_trans with (m := q * b).
+auto with zarith. rewrite Zmult_comm. auto with zarith.
 Qed.
 
-(* Defining property. *)
 
 Require Import QArith.
 
 (* Define the floor function, and prove its characteristic property. *)
 Definition floor (x : Q) : Z := (Qnum x / ' Qden x)%Z.
+
+(* Three distinct characterisations of the floor function. *)
 
 Lemma floor_spec : forall (x:Q) (n:Z),
     inject_Z n <= x  <->  (n <= floor x)%Z.
@@ -61,6 +59,35 @@ generalize (Qnum x) as a. intro a.
 generalize (Qden x) as b. intro b.
 apply floor_spec_Z. auto with zarith.
 Qed.
+
+Lemma floor_spec_lt : forall (x:Q) (n:Z),
+    x < inject_Z n  <->  (floor x < n)%Z.
+Proof.
+  intros x n.
+  pose proof (floor_spec x n).
+  assert (x < (inject_Z n) <-> not ((inject_Z n) <= x)) by (split; auto with qarith).
+  assert ((floor x) < n <-> not (n <= (floor x)))%Z by (split; auto with zarith).
+  tauto.
+Qed.
+
+Lemma floor_spec_alt : forall (x:Q) (n:Z),
+    n = floor x  <->  inject_Z n <= x /\ x < inject_Z n + 1.
+Proof.
+  intros x n.
+  assert (inject_Z n + 1 = inject_Z (n + 1)).
+  rewrite inject_Z_plus. auto with qarith.
+  rewrite H. clear H.
+
+  split; intro.
+    split. apply floor_spec. auto with zarith.
+    apply floor_spec_lt. auto with zarith.
+
+    elim H. intros.
+    assert (n <= floor x)%Z by (apply floor_spec; assumption).
+    assert (floor x < n + 1)%Z by (apply floor_spec_lt; assumption).
+    omega.
+Qed.
+
 
 (* Define the ceiling function, and prove its characteristic property. *)
 Definition ceiling (x : Q) : Z := (-floor(-x))%Z.
