ffd580896becaf2e98b5d2c80038db7e10c7e44d
Mark Dickinson <dickinsm@gmail.com>
2015-07-31 17:02:52 +0200

    Add next_down, next_down_spec, and assorted basic results about next_up and next_down.

--- next_up.v
+++ next_up.v
@@ -12,6 +12,9 @@ Require Import cobinade.
 Require Import twopower_tactics.
 Require Import binary_float.
 
+Notation "[ e ]" := (exist _ e _).
+Notation "' x" := (proj1_sig x).
+
 (* Move this to remedial results. *)
 
 Lemma Zle_ge_succ (x y : Z) : (x <= y  \/  y + 1 <= x)%Z.
@@ -44,14 +47,6 @@ Lemma Qlt_le_pred x y : is_integer x -> is_integer y ->
 Proof.
   setoid_replace (x <= y - 1) with (x + 1 <= y) by (split; intro; rearrange).
   intros; split; now apply Qlt_le_succ.
-Qed.
-
-(* Move these elsewhere *)
-
-Lemma float_lt_trans p (f g h : binary_float p) :
-  (f < g  ->  g < h  ->  f < h)%float.
-Proof.
-  apply Qlt_trans.
 Qed.
 
 (* Let's begin by defining next_up and next_down for positive floats,
@@ -100,7 +95,7 @@ Section NextUpNextDownPositive.
         apply twopower_binade_lt; omega.
     Qed.
 
-    Definition next_up_positive : binary_float p.
+    Definition _next_up_positive : binary_float p.
     Proof.
       refine (float_from_significand_and_exponent
                 p next_up_significand next_up_exponent _);
@@ -114,24 +109,24 @@ Section NextUpNextDownPositive.
         + apply Qlt_le_weak, significand_lt_next_up_significand.
     Defined.
 
-    (* The next two results completely determine next_up_positive:
-       we have f < next_up_positive, and there are no floats
-       between f and next_up_positive. *)
+    (* The next two results completely determine _next_up_positive:
+       we have f < _next_up_positive, and there are no floats
+       between f and _next_up_positive. *)
 
-    Theorem lt_next_up_positive : (f < next_up_positive)%float.
+    Theorem lt_next_up_positive : (f < _next_up_positive)%float.
     Proof.
-      unfold float_lt, next_up_positive; simpl;
+      unfold float_lt, _next_up_positive; simpl;
       change (proj1_sig f) with (proj1_sig f_as_qpos);
       twopower_left; apply significand_lt_next_up_significand.
     Qed.
 
-    Theorem next_up_positive_positive : (0 < next_up_positive)%float.
+    Theorem next_up_positive_positive : (0 < _next_up_positive)%float.
     Proof.
-      apply float_lt_trans with (g := f); try easy; apply lt_next_up_positive.
+      apply float_lt_trans with (y := f); try easy; apply lt_next_up_positive.
     Qed.
 
     Theorem next_up_positive_is_next_up (g : binary_float p) :
-      (g <= f)%float  \/  (next_up_positive <= g)%float.
+      (g <= f)%float  \/  (_next_up_positive <= g)%float.
     Proof.
       destruct (Qlt_le_dec (proj1_sig g) (twopowerQ (binade f_as_qpos))).
       - (* Case g < twopowerQ (binade f_as_qpos) *)
@@ -167,7 +162,7 @@ Section NextUpNextDownPositive.
           * apply floor_spec, Z.le_refl.
         + (* Case (f / 2^exp) + 1 <= (g / 2^exp) *)
           right.
-          unfold float_le, next_up_positive.
+          unfold float_le, _next_up_positive.
           simpl.
           twopower_right.
           unfold next_up_significand.
@@ -219,7 +214,7 @@ Section NextUpNextDownPositive.
       apply twopower_cobinade_le, Z.le_refl.
     Qed.
 
-    Definition next_down_positive : binary_float p.
+    Definition _next_down_positive : binary_float p.
     Proof.
       refine (float_from_significand_and_exponent
                 p next_down_significand next_down_exponent _);
@@ -233,28 +228,28 @@ Section NextUpNextDownPositive.
         + apply next_down_significand_bounded_below.
     Defined.
 
-    Theorem lt_next_down_positive : (next_down_positive < f)%float.
+    Theorem lt_next_down_positive : (_next_down_positive < f)%float.
     Proof.
-      unfold float_lt, next_down_positive; simpl;
+      unfold float_lt, _next_down_positive; simpl;
       change (proj1_sig f) with (proj1_sig f_as_qpos);
       twopower_right; apply next_down_significand_lt_significand.
     Qed.
 
-    Theorem next_down_positive_positive : (0 < next_down_positive)%float.
+    Theorem next_down_positive_positive : (0 < _next_down_positive)%float.
     Proof.
-      unfold float_lt, next_down_positive; simpl; twopower_left;
+      unfold float_lt, _next_down_positive; simpl; twopower_left;
       apply Qlt_le_trans with (y := twopowerQ ('p - 1)).
       + apply twopowerQ_positive.
       + apply next_down_significand_bounded_below.
     Qed.
 
     Theorem next_down_positive_is_next_down (g : binary_float p) :
-      (g <= next_down_positive)%float  \/  (f <= g)%float.
+      (g <= _next_down_positive)%float  \/  (f <= g)%float.
     Proof.
       destruct (Qlt_le_dec (proj1_sig g) (twopowerQ (cobinade f_as_qpos - 1))).
       - (* Case g < 2^(cobinade f - 1) *)
         left; apply Qlt_le_weak, Qlt_le_trans with (1 := q).
-        unfold next_down_positive, next_down_significand, next_down_exponent;
+        unfold _next_down_positive, next_down_significand, next_down_exponent;
         simpl; twopower_left; apply next_down_significand_bounded_below.
       - (* Case 2^(cobinade f - 1) <= g *)
         destruct
@@ -263,7 +258,7 @@ Section NextUpNextDownPositive.
              (ceiling (proj1_sig f_as_qpos / twopowerQ next_down_exponent))).
         + (* Case g / 2^exp <= f / 2^exp - 1. *)
           left.
-          unfold float_le, next_down_positive. simpl.
+          unfold float_le, _next_down_positive. simpl.
           twopower_left.
           setoid_replace (proj1_sig g / twopowerQ next_down_exponent)
           with (inject_Z (ceiling (proj1_sig g /
@@ -373,16 +368,14 @@ Section NextUpNextDown.
     [ left | right | contradict x_nonzero; symmetry]; easy.
   Defined.
 
-  Notation "[ e ]" := (exist _ e _).
-
   Definition next_up (f : nonzero_float) :
     nonzero_float.
   Proof.
     refine
       (
         if float_is_positive f
-        then [next_up_positive _ (proj1_sig f) _ ]
-        else [- (next_down_positive _ (-proj1_sig f) _)]
+        then [_next_up_positive _ (proj1_sig f) _ ]
+        else [- (_next_down_positive _ (-proj1_sig f) _)]
       ).
     - apply float_positive_implies_nonzero, next_up_positive_positive.
     - apply float_negative_nonzero_is_nonzero, float_positive_implies_nonzero,
@@ -470,6 +463,228 @@ Section NextUpNextDown.
           intro q.
           change ((q < q)%Q -> False) with (~ (q < q)%Q).
           auto with qarith.
+  Qed.
+
+  (* Some basic properties. First, next_up is well-defined. *)
+
+  Theorem next_up_morphism (f g : nonzero_float) :
+    ' f == ' g  ->  ' next_up f == ' next_up g.
+  Proof.
+    intro; apply float_le_antisymm; apply next_up_spec;
+    (rewrite H || rewrite <- H); apply next_up_spec;
+    apply float_le_refl.
+  Qed.
+
+  (* And next_up preserves the sign of its input *)
+
+  Theorem next_up_positive f : 0 < 'f -> 0 < ' next_up f.
+  Proof.
+    intro f_positive; apply float_lt_trans with (1 := f_positive).
+    apply next_up_spec, float_le_refl.
+  Qed.
+
+  Theorem next_up_negative f : 'f < 0 -> ' next_up f < 0.
+  Proof.
+    unfold next_up; destruct (float_is_positive f); intro.
+    - (* Case 0 < 'f < 0; contradiction *)
+      assert (@float_lt p 0 0) by (now apply float_lt_trans with (y := ' f)).
+      now contradict H0.
+    - (* Case f' < 0. *)
+      simpl.
+      apply float_neg_lt_flip.
+      apply next_down_positive_positive.
+  Qed.
+
+  (* Now we can also define next_down.  We could do this from first
+     principles, but it's easier to define it in terms of next_up.
+
+     We first define negation at the level of nonzero_float. *)
+
+  Definition nonzero_float_neg (f : nonzero_float) : nonzero_float.
+  Proof.
+    refine [- 'f]; apply float_negative_nonzero_is_nonzero; now destruct f.
+  Defined.
+
+  Lemma nonzero_float_neg_neg (f : nonzero_float) :
+    'nonzero_float_neg (nonzero_float_neg f) == 'f.
+  Proof.
+    unfold nonzero_float_neg, float_eq, float_opp; simpl; ring.
+  Qed.
+
+  Lemma nonzero_float_neg_lt_switch (f g : nonzero_float) :
+    'nonzero_float_neg f < 'g  <->  'nonzero_float_neg g < 'f.
+  Proof.
+    unfold nonzero_float_neg, float_lt; simpl; split; intro; rearrange.
+  Qed.
+
+  Lemma nonzero_float_lt_neg_switch (f g : nonzero_float) :
+    'f < 'nonzero_float_neg g  <->  'g < 'nonzero_float_neg f.
+  Proof.
+    unfold nonzero_float_neg, float_lt; simpl; split; intro; rearrange.
+  Qed.
+
+  Lemma nonzero_float_le_neg_switch (f g : nonzero_float) :
+    'f <= 'nonzero_float_neg g  <->  'g <= 'nonzero_float_neg f.
+  Proof.
+    unfold nonzero_float_neg, float_le; simpl; split; intro; rearrange.
+  Qed.
+
+  Definition next_down (f : nonzero_float) : nonzero_float :=
+    nonzero_float_neg (next_up (nonzero_float_neg f)).
+
+  Lemma nonzero_float_neg_morphism (f g : nonzero_float) :
+    'f == 'g ->  'nonzero_float_neg f == 'nonzero_float_neg g.
+  Proof.
+    intro H; unfold nonzero_float_neg; simpl; now rewrite H.
+  Qed.
+
+  Lemma nonzero_float_neg_positive_is_negative f :
+    0 < 'f  -> 'nonzero_float_neg f < 0.
+  Proof.
+    unfold nonzero_float_neg, float_lt; simpl; intro; rearrange.
+  Qed.
+
+  Lemma nonzero_float_neg_negative_is_positive f :
+    'f < 0  ->  0 < 'nonzero_float_neg f.
+  Proof.
+    unfold nonzero_float_neg, float_lt; simpl; intro; rearrange.
+  Qed.
+
+  Theorem next_down_morphism (f g : nonzero_float) :
+    'f == 'g  ->  'next_down f == 'next_down g.
+  Proof.
+    unfold next_down; intro;
+    now apply nonzero_float_neg_morphism, next_up_morphism,
+        nonzero_float_neg_morphism.
+  Qed.
+
+  Theorem next_down_spec (f g : nonzero_float) :
+    'f <= 'next_down g  <->   'f < 'g.
+  Proof.
+    unfold next_down.
+    rewrite <- nonzero_float_neg_neg at 2.
+    rewrite nonzero_float_neg_lt_switch.
+    rewrite nonzero_float_le_neg_switch.
+    apply next_up_spec.
+  Qed.
+
+  Theorem next_down_positive (f : nonzero_float) :
+    0 < 'f  ->  0 < ' next_down f.
+  Proof.
+    unfold next_down; intro; now apply nonzero_float_neg_negative_is_positive,
+                             next_up_negative, nonzero_float_neg_positive_is_negative.
+  Qed.
+
+  Theorem next_down_negative (f : nonzero_float) :
+    'f < 0  ->  'next_down f < 0.
+  Proof.
+    unfold next_down; intro;
+    now apply nonzero_float_neg_positive_is_negative,
+    next_up_positive, nonzero_float_neg_negative_is_positive.
+  Qed.
+
+  (* Alternative versions of the specs of next_up and next_down. *)
+
+  Lemma float_lt_nge (f g : binary_float p) : f < g  <->  ~(g <= f).
+  Proof.
+    apply Qlt_nge.
+  Qed.
+
+  Lemma float_le_ngt (f g : binary_float p) : f <= g  <->  ~(g < f).
+  Proof.
+    unfold float_le, float_lt.
+    split; intro.
+    now apply Qle_not_lt.
+    now apply Qnot_lt_le.
+  Qed.
+
+  Theorem next_up_spec_alt (f g : nonzero_float) :
+    'g < 'next_up f  <->  'g <= 'f.
+  Proof.
+    rewrite float_le_ngt, float_lt_nge; apply negate_iff, next_up_spec.
+  Qed.
+
+  Theorem next_down_spec_alt (f g : nonzero_float) :
+    'next_down g < 'f  <->  'g <= 'f.
+  Proof.
+    rewrite float_le_ngt, float_lt_nge; apply negate_iff, next_down_spec.
+  Qed.
+
+  (* next_up and next_down are inverses of one another *)
+
+  Theorem next_up_next_down f : ' next_up (next_down f) == ' f.
+  Proof.
+    apply float_le_antisymm.
+    - apply next_up_spec, next_down_spec, float_le_refl.
+    - apply next_down_spec_alt, next_up_spec_alt, float_le_refl.
+  Qed.
+
+  Theorem next_down_next_up f : ' next_down (next_up f) == 'f.
+  Proof.
+    apply float_le_antisymm.
+    - apply next_up_spec_alt, next_down_spec_alt, float_le_refl.
+    - apply next_down_spec, next_up_spec, float_le_refl.
+  Qed.
+
+  (* next_up and next_down are monotonic and injective. *)
+
+  Theorem next_up_eq f g :
+    'f == 'g  <->  'next_up f == 'next_up g.
+  Proof.
+    split; intro H.
+    - apply float_le_antisymm; apply next_up_spec, next_up_spec_alt;
+      rewrite H; apply float_le_refl.
+    - apply float_le_antisymm; apply next_up_spec_alt, next_up_spec;
+      rewrite H; apply float_le_refl.
+  Qed.
+
+  Theorem next_up_le f g :
+    'f <= 'g  <->  'next_up f <= 'next_up g.
+  Proof.
+    split; intro.
+    - now apply next_up_spec, next_up_spec_alt.
+    - now apply next_up_spec_alt, next_up_spec.
+  Qed.
+
+  Theorem next_up_lt f g :
+    'f < 'g  <->  'next_up f < 'next_up g.
+  Proof.
+    split; intro.
+    - now apply next_up_spec_alt, next_up_spec.
+    - now apply next_up_spec, next_up_spec_alt.
+  Qed.
+
+  Theorem next_down_eq f g :
+    'f == 'g  <->  'next_down f == 'next_down g.
+  Proof.
+    split; intro H; apply float_le_antisymm;
+    (apply next_down_spec, next_down_spec_alt
+                             || apply next_down_spec_alt, next_down_spec);
+    rewrite H; apply float_le_refl.
+  Qed.
+
+  Theorem next_down_le f g :
+    'f <= 'g  <->  'next_down f <= 'next_down g.
+  Proof.
+    split; intro;
+    (
+      apply next_down_spec, next_down_spec_alt
+      ||
+      apply next_down_spec_alt, next_down_spec
+    );
+    easy.
+  Qed.
+
+  Theorem next_down_lt f g :
+    'f < 'g  <->  'next_down f < 'next_down g.
+  Proof.
+    split; intro;
+    (
+      apply next_down_spec_alt, next_down_spec
+      ||
+      apply next_down_spec, next_down_spec_alt
+    );
+    easy.
   Qed.
 
 End NextUpNextDown.
