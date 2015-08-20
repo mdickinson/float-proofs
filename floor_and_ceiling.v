ab91277f7e3cb35bc1b1ffbf2a6b084653fee563
Mark Dickinson <dickinsm@gmail.com>
2015-08-20 20:50:27 +0100

    Clean up floor and ceiling main theorems.

--- floor_and_ceiling.v
+++ floor_and_ceiling.v
@@ -9,166 +9,113 @@ Require Import Qabs.
 Require Import remedial.
 Require Import rearrange_tactic.
 
-(* The floor function can be defined in terms of the standard library
-   / operator, which does Euclidean division. *)
-
 Local Open Scope Z.
 
-Definition floor (x : Q) : Z := (Qnum x / ' Qden x).
+(* The floor function can be defined in terms of the standard library
+   / operator, which does Euclidean division with the appropriate
+   semantics when the numerator is negative. *)
+
+Definition floor q := (Qnum q / ' Qden q).
 
 (* To prove the main specification theorem for floor, we first need
    to prove a corresponding theorem at integer level. *)
 
-Lemma floor_spec_Z (a b n : Z) : 0 < b -> (n * b <= a  <->  n <= a / b).
+Lemma _floor_spec_Z a b n : 0 < b -> (n * b <= a  <->  n <= a / b).
 Proof.
-  intro; rewrite Z.mul_comm; split; intro;
-    [ now apply Z.div_le_lower_bound |
-    apply Z.le_trans with (m := b * (a / b));
-      [apply Z.mul_le_mono_pos_l | apply Z.mul_div_le]; easy].
+  intro; rewrite Z.mul_comm; split; intro.
+  - now apply Z.div_le_lower_bound.
+  - eapply Z.le_trans;
+    ((apply Z.mul_le_mono_pos_l; eassumption) || now apply Z.mul_div_le).
 Qed.
 
-(* Here's a trivially-equivalent variant based on < rather than <=. *)
-
-Lemma floor_spec_Z_lt (a b n : Z) : (0 < b) -> (a < n * b  <->  a / b < n).
-Proof.
-  intro; repeat rewrite Z.lt_nge; apply negate_iff; now apply floor_spec_Z.
-Qed.
-
-(* Now we're in a position to give a lemma that complete characterises
-   the floor function.  In fact, we'll give three distinct characterisations.
-
-   To begin with, it's an adjoint to the inclusion of Z into Q.
- *)
+(* Now we can show that floor is an adjoint to the inclusion of Z into Q,
+   a property which completely characterises the floor function. *)
 
 Local Open Scope Q.
 
-Lemma floor_spec (x : Q) (n : Z) : inject_Z n <= x  <->  (n <= floor x)%Z.
+Theorem floor_spec q n : inject_Z n <= q  <->  (n <= floor q)%Z.
 Proof.
-  unfold Qle; rewrite Z.mul_1_r; apply floor_spec_Z; apply Pos2Z.is_pos.
+  unfold Qle; rewrite Z.mul_1_r; apply _floor_spec_Z, Pos2Z.is_pos.
 Qed.
 
 (* It's easier to prove that "floor" is a setoid morphism
    using "floor_spec" than using the definition directly. *)
 
-Add Morphism floor : floor_morphism.
+Add Parametric Morphism : floor
+    with signature Qeq ==> eq as floor_morphism.
 Proof.
-  intros x y x_eq_y; apply Z.le_antisymm; apply floor_spec;
-  [ rewrite <- x_eq_y | rewrite x_eq_y ]; apply floor_spec; apply Z.le_refl.
+  intros q r q_eq_r; apply Z.le_antisymm; apply floor_spec;
+  [ rewrite <- q_eq_r | rewrite q_eq_r ]; apply floor_spec, Z.le_refl.
 Qed.
 
-(* Variant of the first characterisation based on < rather than <=. *)
+(* Variant formulation of floor_spec in terms of < rather than <=. *)
 
-Lemma floor_spec_lt (x : Q) (n : Z) : x < inject_Z n  <->  (floor x < n)%Z.
+Theorem floor_spec_lt q n : q < inject_Z n  <->  (floor q < n)%Z.
 Proof.
-  rewrite Z.lt_nge; rewrite Qlt_nge; apply negate_iff; apply floor_spec.
+  rewrite Z.lt_nge, Qlt_nge; apply negate_iff, floor_spec.
 Qed.
 
-(* The third characterisation describes floor x as the integer n
-   satisfying n <= x < n + 1. *)
+(* It follows from the specification that floor is monotonic. *)
 
-Lemma floor_spec_alt (x : Q) (n : Z) :
-  n = floor x  <->  (inject_Z n <= x < inject_Z n + 1)%Q.
+Theorem floor_monotonic q r : q <= r  ->  (floor q <= floor r)%Z.
 Proof.
-  replace (inject_Z n + 1) with (inject_Z (n + 1)) by
-    (now rewrite inject_Z_plus);
-  split; intro;
-  [
-    split; [apply floor_spec | apply floor_spec_lt]; auto with zarith
-  |
-    apply Z.le_antisymm;
-    [ apply floor_spec | apply Zlt_succ_le; apply floor_spec_lt]; easy
-  ].
-Qed.
-
-Lemma floor_inject (n : Z) : floor (inject_Z n) = n.
-Proof.
-  apply Z.le_antisymm; [ rewrite Zle_Qle | ]; apply floor_spec;
-  [apply Zle_refl | apply Qle_refl].
-Qed.
-
-Lemma floor_monotonic (q r : Q) : q <= r  ->  (floor q <= floor r)%Z.
-Proof.
-  intro; apply floor_spec; apply Qle_trans with (y := q);
-  [apply floor_spec; apply Z.le_refl | easy].
+  intro q_le_r;
+  apply floor_spec, Qle_trans with (2 := q_le_r), floor_spec, Z.le_refl.
 Qed.
 
 (* Define the ceiling function in terms of floor, and prove its characteristic
    properties. *)
 
-Definition ceiling (x : Q) : Z := (-floor(-x))%Z.
+Definition ceiling q := (- floor (- q))%Z.
 
-Add Morphism ceiling : ceiling_morphism.
-  intros x y x_eq_y; unfold ceiling; rewrite x_eq_y; reflexivity.
-Qed.
-
-Lemma ceiling_spec (x : Q) (n : Z) : x <= inject_Z n  <->  (ceiling x <= n)%Z.
+Add Parametric Morphism : ceiling
+    with signature Qeq ==> eq as ceiling_morphism.
 Proof.
-  split; intro; [
-      apply Zle_sign_flip_l; apply floor_spec; rewrite inject_Z_opp;
-        now rewrite <- Qopp_le_mono
-    |
-      rewrite Qopp_le_mono; rewrite <- inject_Z_opp; apply floor_spec;
-        now apply Zle_sign_flip_l
-    ].
+  intros q r q_eq_r; unfold ceiling; now rewrite q_eq_r.
 Qed.
 
+Theorem ceiling_spec q n : q <= inject_Z n  <->  (ceiling q <= n)%Z.
+Proof.
+  split; intro.
+  - apply Zle_sign_flip_l, floor_spec;
+    now rewrite inject_Z_opp, <- Qopp_le_mono.
+  - rewrite Qopp_le_mono, <- inject_Z_opp;
+    now apply floor_spec, Zle_sign_flip_l.
+Qed.
 
-Lemma ceiling_spec_lt (x:Q) (n:Z) : inject_Z n < x  <->  (n < ceiling x)%Z.
+Theorem ceiling_spec_lt q n : inject_Z n < q  <->  (n < ceiling q)%Z.
 Proof.
   rewrite Z.lt_nge; rewrite Qlt_nge; apply negate_iff; apply ceiling_spec.
 Qed.
 
-
-Lemma ceiling_spec_alt (x : Q) (n : Z) :
-  n = ceiling x  <->  inject_Z n - 1 < x /\ x <= inject_Z n.
+Theorem ceiling_monotonic q r : q <= r  ->  (ceiling q <= ceiling r)%Z.
 Proof.
-  replace (inject_Z n - 1) with (inject_Z (n - 1)) by (
-    unfold Z.sub; rewrite inject_Z_plus; auto);
-  split; intro;
-  [
-    split; [apply ceiling_spec_lt | apply ceiling_spec]; auto with zarith
-  |
-    apply Z.le_antisymm;
-    [ apply Z.lt_pred_le; apply ceiling_spec_lt | apply ceiling_spec]; tauto
-  ].
+  intro q_le_r;
+  apply ceiling_spec, Qle_trans with (1 := q_le_r), ceiling_spec, Z.le_refl.
 Qed.
 
+(* Relationship between floor and ceiling. *)
 
-Lemma ceiling_inject (n : Z) : ceiling (inject_Z n) = n.
-Proof.
-  apply Z.le_antisymm; [ | rewrite Zle_Qle]; apply ceiling_spec;
-  [auto with qarith | auto with zarith].
-Qed.
-
-
-Lemma ceiling_monotonic (q r : Q) : q <= r  ->  (ceiling q <= ceiling r)%Z.
-Proof.
-  intro; apply ceiling_spec; apply Qle_trans with (y := r);
-  [ easy | apply ceiling_spec; apply Z.le_refl].
-Qed.
-
-
-Lemma neg_ceiling_is_floor_neg (q : Q) : (- ceiling q)%Z = floor (-q).
+Theorem neg_ceiling_is_floor_neg q : (- ceiling q)%Z = floor (-q).
 Proof.
   apply Z.opp_involutive.
 Qed.
 
-Lemma neg_floor_is_ceiling_neg (q : Q) : (- floor q)%Z = ceiling (-q).
+Theorem neg_floor_is_ceiling_neg q : (- floor q)%Z = ceiling (-q).
 Proof.
   unfold ceiling; now rewrite Qopp_opp.
 Qed.
 
-
-Lemma floor_ceiling_bound (q : Q) : (ceiling q <= floor q + 1)%Z.
+Theorem floor_ceiling_bound q : (ceiling q <= floor q + 1)%Z.
 Proof.
-  apply ceiling_spec; apply Qlt_le_weak; apply floor_spec_lt;
-  apply Zle_lt_succ; apply Z.le_refl.
+  apply ceiling_spec, Qlt_le_weak, floor_spec_lt; omega.
 Qed.
 
-Lemma floor_le_ceiling (q : Q) : (floor q <= ceiling q)%Z.
+Theorem floor_le_ceiling q : (floor q <= ceiling q)%Z.
 Proof.
-  rewrite Zle_Qle; apply Qle_trans with (y := q);
-  [apply floor_spec | apply ceiling_spec]; apply Zle_refl.
+  rewrite Zle_Qle; apply Qle_trans with (y := q).
+  - apply floor_spec, Z.le_refl.
+  - apply ceiling_spec, Z.le_refl.
 Qed.
 
 
@@ -228,9 +175,11 @@ Qed.
 
 Lemma floor_integer (q : Q) : is_integer q <-> inject_Z (floor q) == q.
 Proof.
-  unfold is_integer; split; intro H;
-  [destruct H as [n H]; rewrite <- H; now rewrite floor_inject |
-  now exists (floor q) ].
+  unfold is_integer; split.
+  - destruct 1 as [n n_eq_q]; rewrite <- n_eq_q; apply Qle_antisym.
+    + apply floor_spec, Z.le_refl.
+    + rewrite <- Zle_Qle; apply floor_spec, Qle_refl.
+  - intro; now exists (floor q).
 Qed.
 
 Lemma integer_le_floor (x y : Q) :
@@ -250,9 +199,11 @@ Qed.
 
 Lemma ceiling_integer (q : Q) : is_integer q <-> inject_Z (ceiling q) == q.
 Proof.
-  unfold is_integer; split; intro H;
-  [destruct H as [n H]; rewrite <- H; now rewrite ceiling_inject |
-  now exists (ceiling q)].
+  unfold is_integer; split.
+  - destruct 1 as [n n_eq_q]; rewrite <- n_eq_q; apply Qle_antisym.
+    + rewrite <- Zle_Qle; apply ceiling_spec, Z.le_refl.
+    + apply ceiling_spec, Z.le_refl.
+  - intro; now exists (ceiling q).
 Qed.
 
 (* As a corollary of the above characterization of is_integer in terms of an
